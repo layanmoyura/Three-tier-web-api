eb0debeca8efc7d9540f3d2c9960938cab96e299
layanmoyura <layanmoyura@gmail.com>
2023-11-01 20:35:38 +0530

    data refernce remove for models

--- PresentationLayer/Soft-Gallery-Project-repo/Model/CourseModel.cs
+++ PresentationLayer/Soft-Gallery-Project-repo/Model/CourseModel.cs
@@ -1,17 +1,12 @@
-﻿using DataAccessLayer.Entity;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-
-
-namespace PresentationLayer.Models
+﻿namespace PresentationLayer.Models
 {
     public class CourseModel
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        
         public int CourseID { get; set; }
         public string Title { get; set; }
 
         public int Credits { get; set; }
-        public List<Enrollment> Enrollments { get; set; }
+        
     }
 }
--- PresentationLayer/Soft-Gallery-Project-repo/Model/EnrollmentModel.cs
+++ PresentationLayer/Soft-Gallery-Project-repo/Model/EnrollmentModel.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DataAccessLayer.Entity;
-
+﻿
 namespace PresentationLayer.Models
 {
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
     public class EnrollmentModel
     {
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
         public Grade? Grade { get; set; }
-        public Course Course { get; set; }
-        public Student Student { get; set; }
+        
 
 
     }
